857a8a129bdc63c2c2669d2622a8089a142e5690
Yam <maxiaolion@gmail.com>
2020-03-21 16:53:36 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,21 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
-context.arc(100, 100, 20, 0, Math.PI*2);
+//Light Configuration
+var lightConfig = {
+    color: '#ffff00',
+    radius: 20,
+    x: 100,
+    y: 100,
+};
+
+function drawLight(){
+    context.beginPath();
+    context.arc(lightConfig.x, lightConfig.y, lightConfig.radius, 0, Math.PI*2, false);
+    context.stroke();
+    context.fillStyle = lightConfig.color;
+    context.fill();
+    context.closePath();
+}
+
+drawLight();

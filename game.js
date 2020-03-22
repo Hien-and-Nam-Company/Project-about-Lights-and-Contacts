1b228a7f1f34b9843d457b9a46507467202a1a45
Yam <maxiaolion@gmail.com>
2020-03-22 20:01:27 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,45 +1,61 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
-function drawContacts(){
-    //Tạo các đối tượng công tắc (tọa độ x, tọa độ y, bật?)
-    contact1 = new Contact(10, 10, false);
-    contact2 = new Contact(140, 10, true);
-    contact3 = new Contact(10, 210, true);
-    contact4 = new Contact(140, 265, true);
-    contact5 = new Contact(10, 370, true);
-    //Vẽ 5 công tắc
-    contact1.draw();
-    contact2.draw();
-    contact3.draw();
-    contact4.draw();
-    contact5.draw();
+//Tạo các đối tượng công tắc (tọa độ x, tọa độ y, bật?)
+var contactList = [];
+    contactList[0] = new Contact(10, 10, false, 0);
+    contactList[1] = new Contact(140, 10, true, 1);
+    contactList[2] = new Contact(10, 210, true, 2);
+    contactList[3] = new Contact(140, 265, true, 3);
+    contactList[4] = new Contact(10, 370, true, 4);
+
+//Vẽ TẤT CẢ công tắc
+function drawContacts(){  
+    contactList.forEach(function(currentContact){
+        currentContact.draw();
+    })
 }
 
+//Tạo các đối tượng bóng đèn (vị trí, 2 công tắc mà nó chịu tác động)
+var lightList = [];
+lightList[0] = new Light(100, 65,  contactList[0], contactList[1]);
+lightList[1] = new Light(100, 260, contactList[2], contactList[3]);
+lightList[2] = new Light(100, 370, contactList[3], contactList[4]);
+
+//Vẽ TẤT CẢ bóng đèn
 function drawLights(){
-    //Thông tin bóng đèn (vị trí, 2 công tắc mà nó chịu tác động)
-    light1 = new Light(100, 65, contact1, contact2);
-    light2 = new Light(100, 260, contact3, contact4);
-    light3 = new Light(100, 370, contact4, contact5);
-
-    //Vẽ 3 bóng đèn
-    light1.draw(light1.firstContact.status, light1.secondContact.status);
-    light2.draw(light2.firstContact.status, light2.secondContact.status);
-    light3.draw(light3.firstContact.status, light3.secondContact.status);
+    lightList.forEach(function(currentLight){
+        currentLight.draw();
+    })
 }
 
-//Khởi tạo hàm vẽ TẤT CẢ items
-function draw(){
-    drawContacts();
-    drawLights();   
-    // drawLinkButton();
+//Tạo đối tượng Link
+link = new Link(250, 50, false);
+
+//Vẽ đối tượng Link
+function drawLinkButton(){
+    link.drawButton();
+}
+
+//Kiểm tra nhấp chuột
+var mouseX = null, mouseY = null;
+function mousePressed(){
+    canvas.onmousedown = function(mousePosition){
+        mouseX = mousePosition.offsetX;
+        mouseY = mousePosition.offsetY;  
     
+        contactList.forEach(function(currentContact){
+            currentContact.clicked();
+        })
+        drawLights();
+
+        link.clicked();
+    }
 }
 
-//Gọi hàm vẽ TẤT CẢ đối tượng
-draw();
+//Vẽ TẤT CẢ đối tượng
+drawContacts();
+drawLights();
+drawLinkButton();
 
-//Gọi hàm kiểm tra click chuột
-// checkMouseClicked();
-
-
+mousePressed();

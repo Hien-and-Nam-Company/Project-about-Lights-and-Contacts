98d504a0e0d678f4be641a408be8cc7c67a64de0
Yam <maxiaolion@gmail.com>
2020-03-22 22:42:06 +0700

    Update game.js
    Mới nhất!

--- game.js
+++ game.js
@@ -3,15 +3,16 @@ var context = canvas.getContext('2d');
 
 //Tạo các đối tượng công tắc (tọa độ x, tọa độ y, bật?)
 var contactList = [];
-    contactList[0] = new Contact(10, 10, false, 0);
-    contactList[1] = new Contact(140, 10, true, 1);
-    contactList[2] = new Contact(10, 210, true, 2);
-    contactList[3] = new Contact(140, 265, true, 3);
-    contactList[4] = new Contact(10, 370, true, 4);
+    contactList[0] = new Contact(10, 10, false);
+    contactList[1] = new Contact(140, 10, true);
+    contactList[2] = new Contact(10, 210, true);
+    contactList[3] = new Contact(140, 265, true);
+    contactList[4] = new Contact(10, 370, true);
 
 //Vẽ TẤT CẢ công tắc
-function drawContacts(){  
+function drawContacts(){ 
     contactList.forEach(function(currentContact){
+        currentContact.strokeColor = '#000000';
         currentContact.draw();
     })
 }
@@ -30,7 +31,9 @@ function drawLights(){
 }
 
 //Tạo đối tượng Link
+var isLinkFlag = false;
 link = new Link(250, 50, false);
+var linkTarget = [null, null];
 
 //Vẽ đối tượng Link
 function drawLinkButton(){
@@ -43,12 +46,23 @@ function mousePressed(){
     canvas.onmousedown = function(mousePosition){
         mouseX = mousePosition.offsetX;
         mouseY = mousePosition.offsetY;  
-    
-        contactList.forEach(function(currentContact){
-            currentContact.clicked();
-        })
-        drawLights();
+        
+        if(!isLinkFlag){
+            contactList.forEach(function(currentContact){
+                currentContact.clicked();
+            })
+            drawLights();
 
+            if(linkTarget[0] == null || linkTarget[1 == null]){
+                linkTarget[0] = null;
+                linkTarget[1] = null;
+                drawContacts();
+            }
+        } else{
+            contactList.forEach(function(currentContact){
+                currentContact.clickedLinkTarget();
+            })      
+        }
         link.clicked();
     }
 }
